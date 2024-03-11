08635ee9157a57e8008b0c894e129382ff8cbf81
rostipostik <132700854+rostipostik@users.noreply.github.com>
2024-03-11 17:23:47 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
-    $('.testimonials-slider').slick();
+    $('.testimonials-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:"<img class='prev slick-btn' src='./img/arrow-right.svg'>",
+        nextArrow:"<img class='next slick-btn' src='./img/arrow-left.svg'>"
+    });
     $('.slider-avatars').slick();
 });
