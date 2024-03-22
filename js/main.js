1fd2fa215cfb27d360da39b7c994dc295d75af86
rostipostik <132700854+rostipostik@users.noreply.github.com>
2024-03-22 17:48:49 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -6,5 +6,11 @@ $(document).ready(function () {
         prevArrow:"<img class='next slick-btn' src='./img/arrow-right.svg'>",
         nextArrow:"<img class='prev slick-btn' src='./img/arrow-left.svg'>"
     });
-    $('.slider-avatars').slick();
+    $('.slider-avatars').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '60px',
+    });
 });
